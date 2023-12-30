b9bda734b25b51ce4dd1334747dd899cbc9ad9ec
gorbatykh <93544537+KingKord@users.noreply.github.com>
2023-12-30 21:49:08 +0300

    Time: 3 ms (66.00%), Space: 2.7 MB (93.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -0,0 +1,27 @@
+
+func isPalindrome(s string) bool {
+	var (
+		left  = 0
+		right = len(s) - 1
+	)
+
+	for left < right {
+				if !unicode.IsLetter(rune(s[left])) && !unicode.IsDigit(rune(s[left])) {
+			left++
+			continue
+		}
+		if !unicode.IsLetter(rune(s[right]))&& !unicode.IsDigit(rune(s[right])) {
+			right--
+			continue
+		}
+
+		if unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])) {
+			return false
+		} else {
+			left++
+            right--
+		}
+	}
+
+	return true
+}
