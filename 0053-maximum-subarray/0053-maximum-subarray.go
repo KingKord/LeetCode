e6b755c17d0b93416a0f27e7e019c85ed6d0a4c3
gorbatykh <93544537+KingKord@users.noreply.github.com>
2024-01-07 10:26:17 +0300

    Time: 90 ms (50.15%), Space: 8.2 MB (63.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,27 +1,14 @@
 func maxSubArray(nums []int) int {
 	var (
-		sum = nums[0]
-		max = sum
+		sum = 0
+		max = -10000
 	)
 
-	for i := 1; i < len(nums); i++ {
-		val := nums[i]
-		if val > 0 {
-			sum = Max(val, val+sum)
-			max = Max(sum, max)
-		} else {
-			if sum < val {
-				sum = val
-			} else if sum+val > 0 {
-				sum += val
-			} else {
-				max = Max(sum, max)
-				sum = -100000
-			}
-
-			max = Max(sum, max)
-		}
+	for _, num := range nums {
+		sum = Max(num, sum+num)
+		max = Max(max, sum)
 	}
+
 	return max
 }
 
