8ef9f83fb5bf854b3b244c5ef56f839523ed8683
gorbatykh <93544537+KingKord@users.noreply.github.com>
2024-01-15 10:36:16 +0300

    Time: 38 ms (5.49%), Space: 5.9 MB (94.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -1,79 +1,26 @@
 func topKFrequent(nums []int, k int) []int {
 	var (
-		heap       []int
-		idxToValue = make(map[int]int)
-		valueToIdx = make(map[int]int)
+		hashmap = make(map[int]int)
+		res     = make([]int, k)
 	)
 
 	for _, num := range nums {
-		idx, ok := valueToIdx[num]
-		if !ok {
-			heap = append(heap, 1)
-			idxToValue[len(heap)-1] = num
-			valueToIdx[num] = len(heap) - 1
-		} else {
-			heap[idx]++
-
-			parentIdx := (idx - 1) / 2
-			for heap[parentIdx] < heap[idx] {
-				swap(heap, idxToValue, valueToIdx, parentIdx, idx)
-
-				idx = parentIdx
-				parentIdx = (idx - 1) / 2
-			}
-		}
+		hashmap[num]++
 	}
 
-	ans := make([]int, k)
 	for i := 0; i < k; i++ {
-		ans[i] = idxToValue[0]
+		count := 0
+		el := 0
 
-		swap(heap, idxToValue, valueToIdx, 0, len(heap)-1)
-		heap = heap[:len(heap)-1]
-		idx, left, right := 0, 1, 2
-
-		for {
-
-			if left > len(heap)-1 {
-				break
-			} else if left == len(heap)-1 {
-				max := heap[left]
-				if heap[idx] < max {
-					swap(heap, idxToValue, valueToIdx, idx, left)
-				}
-				break
+		for key, occurrence := range hashmap {
+			if occurrence > count {
+				el = key
+				count = occurrence
 			}
-			if heap[left] > heap[right] {
-
-				max := heap[left]
-				if heap[idx] >= max {
-					break
-				}
-
-				swap(heap, idxToValue, valueToIdx, idx, left)
-				idx = left
-			} else {
-				max := heap[right]
-				if heap[idx] >= max {
-					break
-				}
-
-				swap(heap, idxToValue, valueToIdx, idx, right)
-				idx = right
-			}
-
-			left = 2*idx + 1
-			right = 2*idx + 2
 		}
+		res[i] = el
+		delete(hashmap, el)
 	}
-	return ans
-}
 
-func swap(heap []int, idxToValue map[int]int, valueToIdx map[int]int, parentIdx int, idx int) {
-	heap[idx], heap[parentIdx] = heap[parentIdx], heap[idx]
-
-	child, parent := idxToValue[idx], idxToValue[parentIdx]
-	valueToIdx[child], valueToIdx[parent] = valueToIdx[parent], valueToIdx[child]
-
-	idxToValue[idx], idxToValue[parentIdx] = parent, child
+	return res
 }
