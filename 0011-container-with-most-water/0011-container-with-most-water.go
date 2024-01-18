5dd6d7b5edc8910c1528f1ddc3df658be4effc5d
gorbatykh <93544537+KingKord@users.noreply.github.com>
2024-01-18 20:10:22 +0300

    Time: 111 ms (15.62%), Space: 9.7 MB (6.96%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.go
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -1,27 +1,19 @@
-
 func maxArea(height []int) int {
 	var (
 		L   = 0
-		R   = len(height) - 1
-		max = 0
+		R = len(height) - 1
+		m = 0
 	)
 
 	for L <= R {
 		if height[L] < height[R] {
-			max = Max(max, (R-L)*height[L])
+			m = max(m, (R-L)*height[L])
 			L++
 		} else {
-			max = Max(max, (R-L)*height[R])
+			m = max(m, (R-L)*height[R])
 			R--
 		}
 	}
 
-	return max
-}
-
-func Max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return m
 }
